2647c4cf4040b808b33c1ae208e52998d4d9d80a
patriciamarcela <130873180+patriciamarcela@users.noreply.github.com>
2023-05-21 07:59:26 -0300

    Add files via upload
    Es una tarea de javascriipt con la utilizacion de variables nombre-apellido

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+let nombre= "Pedro" ;
+console.log("nombre", nombre);
+
+let apellido= "Garcia";
+console.log("Apellido", apellido);
+
+let suma= (nombre+  apellido)
+console.log(suma)
+
+
+let nota1= 5;
+let nota2= 7;
+let nota3= 9;
+
+function calcularPromedio(){
+    let promedio= (nota1 + nota2 + nota3)/3
+    return promedio}
+
+    let resultado = calcularPromedio();
+console.log(resultado);
